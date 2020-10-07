2a470c673d8dbb52a4df5c03f9230dd61b1ab1f6
kanchanrikhe-831 <71336463+kanchanrikhe-831@users.noreply.github.com>
2020-10-07 16:48:38 -0400

    SENG8040 : Assignment #1
    Source code for SENG8040
    Test Project for SENG8040

---
+++ SENG8040/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENG8040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter 1.Enter Trangle Dimansion" + "\n" + "2. Exit ");
+            var input = Console.ReadLine();
+
+            if(Convert.ToInt32(input) ==1)
+            { 
+                while(true)
+                { 
+                    Console.WriteLine("Enter First Dimansion as Integer");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Second Dimansion as Integer");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Third Dimansion as Integer");
+                    int num3 = Convert.ToInt32(Console.ReadLine());
+
+                    if(TriangleSolver.ValidateTrangle(num1,num2,num3))
+                    { 
+                            string trangleType =  TriangleSolver.GetTrangleType(num1, num2, num3);
+                            Console.WriteLine("Type of Trangle is :  " + trangleType);
+                            Console.WriteLine("Press any key X to exit program Or Enter to continue");
+                            string exitChar = Console.ReadLine();
+                            if(exitChar.ToUpper()== "X")
+                            {
+                                break;                    
+                            }
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Input Doesn't form Trangle");
+                    }
+                }
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        
+    }
+}
---
+++ SENG8040/TriangleSolver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENG8040
+{
+    public static class TriangleSolver
+    {
+        /// <summary>
+        /// Validate if input forms Trangle
+        /// </summary>
+        /// <param name="side1"></param>
+        /// <param name="side2"></param>
+        /// <param name="side3"></param>
+        /// <returns></returns>
+
+        public static bool ValidateTrangle(int side1, int side2, int side3)
+        {
+            // check condition 
+            if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+                return false;
+            else
+                return true;
+        }
+
+      
+        /// <summary>
+        /// Check Type of Trangle
+        /// </summary>
+        /// <param name="side1"></param>
+        /// <param name="side2"></param>
+        /// <param name="side3"></param>
+        /// <returns></returns>
+        public static string GetTrangleType(int side1, int side2, int side3)
+        {
+            string outputString = string.Empty;
+
+            if(side1 == side2 && side2 == side3)
+            {
+                return "Equilateral";
+            }
+            else if (side1 == side2 || side1 == side3 || side2 == side3)
+            {
+                return "Isosceles"; 
+            }
+            else
+            {
+                return "Scalene";
+            }
+
+        }
+    }
+}
